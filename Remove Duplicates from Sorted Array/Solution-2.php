8d6005e11bfafda2cdcfc0846de79e8412925601
Mohammad Altal <mohammad.altal@lyveglobal.com>
2024-08-20 01:41:55 +0400

    Remove Duplicates from Sorted Array

---
+++ Remove Duplicates from Sorted Array/Solution-2.php
@@ -0,0 +1,26 @@
+<?php
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer
+     */
+    function removeDuplicates(&$nums) {
+        $j = 1;
+        $count = 1;
+        for ($i = 1; $i < count($nums); $i++) {
+            if ($nums[$i] == $nums[$i - 1]) {
+                $count++;
+            } else {
+                $count = 1;
+            }
+
+            if ($count <= 1) {
+                $nums[$j] = $nums[$i];
+                $j++;
+            }
+        }
+
+        return $j;
+    }
+}
